f4e6105c0d6d8c733a5570a214381621c76beabd
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-13 18:52:00 +0200

    fix #1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rémy Kaloustian
+ * Date: 13/04/2016
+ * Time: 18:38
+ */?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" src="css/foundation.css">
+        <title>Epic Projects</title>
+    </head>
+    <body>
+
+    <h1>U wot mate</h1>
+    </body>
+
+
+
+</html>
