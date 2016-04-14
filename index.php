594c04d839ac4390b7f06bd0c1377a827db56316
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-14 08:07:08 +0200

    fix #6

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <head>
         <link rel="stylesheet" href="css/foundation.css">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/hover.css">
+
 
 
 
@@ -23,7 +25,14 @@
         <h1 id="home_title" class="centered">EPIC PROJECTS</h1>
         <h2 id="home_catchphrase" class="centered">Lead your projects like a boss.</h2>
 
-        <a class="download centered" id="landing_download">Download Epic Projects</a>
+        <a class="download centered hvr-shutter-out-vertical large-4 large-centered columns" id="landing_download">Download Epic Projects</a>
+
+    </section>
+
+    <section id="details_section" class="large-12 medium-12 small-12">
+        <h4 id="details_title" class="varela centered large-centered large-8 columns" >Epic Projects is the project management application you need, and the one you deserve too.</h4>
+        <p id="details_text" class="varela centered large-centered medium-centered small-centered columns">Epic Projects allows you to manage your project during all its lifespan. This is not a traditional Kanban-based app.
+            This is an application whose interface is clearly separated to build your project step by step.</p>
 
     </section>
     </body>
