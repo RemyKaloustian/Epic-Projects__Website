bde2a1bdfd9c9c9ce39320af1fef360536e7d824
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-14 09:56:24 +0200

    Seems finished :)

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/hover.css">
 
-
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="js/scroller.js"></script>
 
         <script src="js/modernizr.js"></script>
         <title>Epic Projects</title>
@@ -25,18 +25,19 @@
         <h1 id="home_title" class="centered">EPIC PROJECTS</h1>
         <h2 id="home_catchphrase" class="centered">Lead your projects like a boss.</h2>
 
-        <a class="download centered hvr-shutter-out-vertical large-4 large-centered columns" id="landing_download">Download Epic Projects</a>
+        <a class="download centered hvr-shutter-out-vertical large-4 large-centered columns" id="landing_download" href="#tuto_section">
+            Download Epic Projects</a>
 
     </section>
 
-    <section id="details_section" class="large-12 medium-12 small-12">
+    <section id="details_section" class=" section_title large-12 medium-12 small-12">
         <h4 id="details_title" class="varela centered large-centered large-8 columns" >Epic Projects is the project management application you need, and the one you deserve too.</h4>
         <p id="details_text" class="varela centered large-centered medium-centered small-centered columns">Epic Projects allows you to manage your project during all its lifespan. This is not a traditional Kanban-based app.
             This is an application whose interface is clearly separated to build your project step by step.</p>
 
         <div class="large-2 columns empty">|</div>
 
-        <div class="large-2 columns divs">
+        <div class="large-2 columns ">
             <img class="details_pics" src="pictures/people.png">
             <h5 class="centered varela">Stay organized</h5>
             <p class="centered varela">In Epic Projects, every project is divided in four sections :
@@ -45,7 +46,7 @@
 
         <div class="large-1 columns empty" >|</div>
 
-        <div class="large-2 columns divs">
+        <div class="large-2 columns ">
             <img class="details_pics" src="pictures/hours.png">
             <h5 class="centered varela">Available 24/7</h5>
             <p class="centered varela">Epic Projects is built for individual projects, so there is no need for an internet connection.
@@ -54,7 +55,7 @@
 
         <div class="large-1 columns empty">|</div>
 
-        <div class="large-2 columns divs">
+        <div class="large-2 columns ">
             <img class="details_pics" src="pictures/business.png">
             <h5 class="centered varela">Success incoming</h5>
             <p class="varela centered">Thanks to Epic Projects, you can now boost your ability to organize your work and become more efficient.
@@ -64,7 +65,65 @@
 
         <div class="large-1 columns empty">|</div>
 
+    </section >
+
+
+    <section id="tuto_section" class="large-12 medium-12 small-12">
+        <h4 id="tuto_title" class=" varela centered large-centered large-8 columns" >Get ready for the Epic.</h4>
+        <div class="large-2 columns empty">|</div>
+
+        <div class="large-4 columns varela">
+            <p>Download the zip file.</p>
+            <p>Extract the zip wherever you want.</p>
+            <p>Click on the Epic Projects shortcut</p>
+            <p>Congratulations, your app is ready to work, and you are ready to become GREAT.</p>
+        </div>
+
+        <div class="large-4 columns hvr-shutter-out-horizontal">
+            <a id="lower_download" class="" href="pictures/danse.gif">Download Epic Projects</a>
+
+        </div>
+
+        <div class="large-2 columns empty">|</div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br><br>
+        <br>
+        <div class="large-3 columns varela centered bordered">Brainstorming</div>
+        <div class="large-3 columns varela centered bordered">Training</div>
+        <div class="large-3 columns varela centered bordered">Assignments</div>
+        <div class="large-3 columns varela centered ">Maintenance</div>
+
+
     </section>
+
+    <section id="bug_section">
+        <h4 id="bug_title" class="centered varela white large-centered large-8 columns">Report a bug</h4>
+
+        <p id="bug_text" class="varela centered large-centered large-8 columns white">One day, there will be a more advanced mail service. But for now,
+            send your reports to remy.kaloustian@protonmail.com.</p>
+        <img id="danger" src="pictures/danger.png">
+        <p class="centered varela white large-6 large-centered columns ">Please be specific and precise when describing the bug you encountered. Just saying "Your app bugs" won't help.
+        Make sure to specify the window you were on, the section you were on, what you did, what happened,
+            and what you think should have happened.</p>
+
+    </section>
+
+    <footer class="varela centered white">
+        <p>This website is built by <a class="inline" href="http://remykaloustian.com/" target="_blank">Rémy Kaloustian.</a> </p>
+        <p>Epic Project is made by <a class="inline"  href="http://remykaloustian.com/" target="_blank">Rémy Kaloustian </a>with C#,
+            XAML, and Visual Studio (best IDE on the planet by the way).</p>
+    </footer>
+
     </body>
 
 
